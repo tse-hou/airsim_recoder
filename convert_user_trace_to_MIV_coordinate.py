2eb000002c0dae25b5ef6fb40f1349c1e8fefc46
tsehou <zackl852l@gmail.com>
2021-10-06 15:08:06 +0800

    add arg parse in convert_user_trace_to_MIV_coordinate.py

--- convert_user_trace_to_MIV_coordinate.py
+++ convert_user_trace_to_MIV_coordinate.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from sys import argv
 
-df = pd.read_csv("pose.csv")
+input_pose_trace = argv[1]
+output_pose_trace = argv[2]
+df = pd.read_csv(input_pose_trace)
 df['y'] = -df['y']
 df['z'] = -df['z']
 df['yaw'] = -df['yaw']
@@ -9,4 +12,4 @@ df = df.drop(columns=['t', 'valid'])
 
 new_df = pd.DataFrame(data={'X': df['x'].values, 'Y': df['y'].values, 'Z': df['z'].values,
                             'Yaw': df['yaw'].values, 'Pitch': df['pitch'].values, 'Roll': df['roll'].values})
-new_df.to_csv("converted_pose.csv", index=False)
+new_df.to_csv(output_pose_trace, index=False)
